f051523bc186aec2898ee77e5e22e20c7b80b8b0
luisrodriguesbastos <rodriguesbastos@gmail.com>
2017-02-08 16:10:43 -0200

    minutrade #1

--- index.php
+++ index.php
@@ -1,48 +1,65 @@
 <html>
     <head>
         <link rel="stylesheet" type="css/text" href="test.css"/>
-        <script type="text/javascript" src="test.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <form action="action_page.php" method="POST" name="form1">
-            CPF:<br>
-            <input maxlength="14" type="text" name="cpf" id="cpf" value="" onKeyPress="MascaraCPF(form1.cpf);">
-            <br>
-            E-mail:<br>
-            <input type="email" name="email" id="email" value="">
-            <br><br>
-            Name:<br>
-            <input type="text" name="name" id="name" value="">
-            <br>
-            Fone:<br>
-            <input type="button" name="more" id="more" value="+">
-            <div id="telefone">
-                <input maxlength="15" type="text" name="fone" ite="1" id="fone_1" value="" onKeyPress="MascaraTelefone(form1.fone);" >
-            </div>
-            <br>
-            Estado Civil:<br>
-            <select name="marital_status" id="marital_status" class="selectpicker" prompt="Selecione uma opção">
-                <option value="0">Selecione uma opção</option>
-                <option value="1">Solteiro</option>
-                <option value="2">Casado</option>
-                <option value="3">Divorciado</option>
-            </select>
-            <br><br>
-            <input type="submit" value="Cadastrar">
-            <input type="hidden" name="type" id="type" value="cadastro">
-        </form> 
-
         <form action="action_page.php" method="POST" name="form2">
-            CPF:<br>
-            <input maxlength="14" type="text" name="cpf" id="cpf" value="" onKeyPress="MascaraCPF(form2.cpf);">
-            <br><br>
-            <input type="submit" value="Buscar">
             <input type="hidden" name="type" id="type" value="busca">
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <td>CPF:</td>
+                        <td><input maxlength="14" type="text" name="cpf" id="cpf" value="" onKeyPress="MascaraCPF(form2.cpf);"></td>
+                        <td><input type="submit" value="Buscar"></td>
+                    </tr>
+                </tbody>
+            </table>
         </form>
+        <form action="action_page.php" method="POST" name="form1">
+            <input type="hidden" name="type" id="type" value="cadastro">
+            <table border="2">
+                <tbody>
+                    <tr>
+                        <td>CPF:</td>
+                        <td><input maxlength="14" type="text" name="cpf" id="cpf" value="" onKeyPress="MascaraCPF(form1.cpf);"></td>
+                    </tr>
+                    <tr>
+                        <td>E-mail:</td>
+                        <td><input type="email" name="email" id="email" value=""></td>
+                    </tr>
+                    <tr>
+                        <td>Name:</td>
+                        <td><input type="text" name="name" id="name" value=""></td>
+                    </tr>
+                    <tr>
+                        <td>Fone:
+                            <input type="button" name="more" id="more" value="+">
+                            <input type="button" name="less" id="less" value=" - ">
+                        </td>
+                        <td><div id="telefone">
+                                <input maxlength="15" type="text" name="fone" ite="1" id="fone_1" value="" onKeyPress="MascaraTelefone(form1.fone);" >
+                            </div></td>
+                    </tr>
+                    <tr>
+                        <td>Estado Civil:</td>
+                        <td><select name="marital_status" id="marital_status" class="selectpicker" prompt="Selecione uma opção">
+                                <option value="0">Selecione uma opção</option>
+                                <option value="1">Solteiro</option>
+                                <option value="2">Casado</option>
+                                <option value="3">Divorciado</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: center"><input type="submit" value="Cadastrar"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>         
     </body>
 </html>
 <script>
@@ -51,6 +68,9 @@
         var iteracao = parseInt(ultimo) + 1;
         var novo = '<br /><input maxlength="15" type="text" name="fone_' + (iteracao) + '" ite="' + (iteracao) + '" id="fone_' + (iteracao) + '" value="" onKeyPress="MascaraTelefone(form1.fone_' + (iteracao) + ');" >'
         jQuery("div[id='telefone']").append(novo);
+    });
+    jQuery("input[name='less']").click(function () {
+        jQuery("input[name*='fone_']").last().remove();
     });
 
 
